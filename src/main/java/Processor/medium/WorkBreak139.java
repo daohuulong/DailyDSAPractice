ea3410ec0ac4317b7bdc8d4683f5036ae9ec939f
longhuudao <daohuulong@gmail.com>
2025-07-04 22:59:40 +0700

    Create WorkBreak139.java

---
+++ src/main/java/Processor/medium/WorkBreak139.java
@@ -0,0 +1,56 @@
+package src.main.java.Processor.medium;
+
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+ *
+ * Note that the same word in the dictionary may be reused multiple times in the segmentation.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "leetcode", wordDict = ["leet","code"]
+ * Output: true
+ * Explanation: Return true because "leetcode" can be segmented as "leet code".
+ * Example 2:
+ *
+ * Input: s = "applepenapple", wordDict = ["apple","pen"]
+ * Output: true
+ * Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+ * Note that you are allowed to reuse a dictionary word.
+ * Example 3:
+ *
+ * Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+ * Output: false
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 300
+ * 1 <= wordDict.length <= 1000
+ * 1 <= wordDict[i].length <= 20
+ * s and wordDict[i] consist of only lowercase English letters.
+ * All the strings of wordDict are unique.
+ */
+public class WorkBreak139 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if (s == null || wordDict == null || wordDict.isEmpty() || s.isEmpty()) {
+            return false;
+        }
+        HashSet<String> set = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= i; j++) {
+                if (set.contains(s.substring(j-1, i)) && dp[j - 1]) {
+                    dp[i] = true;
+                }
+            }
+        }
+        // System.out.println(Arrays.toString(dp));
+        return dp[s.length()];
+    }
+}
